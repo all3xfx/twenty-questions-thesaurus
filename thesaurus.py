97d30fd30da3cacb65417fa098a046e6e4439ab0
nguyenkvi <nguyenkvi@gmail.com>
2016-01-15 21:25:26 -0600

    Setting up Flask server

--- thesaurus.py
+++ thesaurus.py
@@ -1,9 +1,23 @@
-from flask import Flask
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# configuration
+DATABASE = '/tmp/thesaurus.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# creates app
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# connects to database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
+# runs app
 if __name__ == '__main__':
     app.run()
