c66d0dad5a33361d8c52e7bb5bba6dd0902963ca
nguyenkvi <nguyenkvi@gmail.com>
2016-01-16 12:20:46 -0600

    Adding routes

--- ThesaurusServer.py
+++ ThesaurusServer.py
@@ -18,10 +18,32 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+
 # routes
 @app.route('/')
 def index():
+    """
+    Renders index page
+    """
     return render_template('index.html')
+
+
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    """
+    Renders static pages
+    """
+    return render_template('%s.html' % page_name)
+
+
+@app.route('/pos', methods=['POST'])
+def determine_pos():
+    return redirect(url_for('determine_similar_word'))
+
+
+@app.route('/simword', methods=['POST'])
+def determine_similar_word():
+    return redirect(url_for('determine_similar_word'))
 
 # runs app
 if __name__ == '__main__':
